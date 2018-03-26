2856ef63cef07a8c0827d0bfaebaf72094247ffb
linnal <erindajaupi@gmail.com>
2018-03-26 19:11:58 +0200

    intro to classes + added another function

--- rectangle_area/src/main.rs
+++ rectangle_area/src/main.rs
@@ -8,13 +8,17 @@ impl Rectangle {
     fn area(&self) -> u32 {
         self.width * self.height
     }
+
+    fn greater_than(&self, rect: &Rectangle) -> bool {
+        self.area() > rect.area()
+    }
 }
 
 fn main() {
-    let rect = Rectangle{
-        width: 30,
-        height: 20
-    };
+    let rect = Rectangle{ width: 30, height: 20};
+    let rect2 = Rectangle{ width: 50, height: 20};
+    
     println!("Given {:?}", rect);
     println!("Area is {:?}", rect.area());
+    println!("rect is greater than rect2? {:?}", rect.greater_than(&rect2));
 }
