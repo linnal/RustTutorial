bcaedd9cc4c8c8567045f8002ba046f94fe957cb
linnal <erindajaupi@gmail.com>
2018-05-11 09:53:24 +0200

    minigrep exercise

---
+++ 0009_minigrep/src/lib.rs
@@ -0,0 +1,45 @@
+use std::fs::File;
+use std::io::prelude::*;
+use std::error::Error;
+
+pub fn run(config: Config) -> Result<(), Box<Error>> {
+    let mut file = File::open(config.filename)?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+
+    for line in search(&config.query, &contents) {
+        println!("{}", line);
+    }
+
+    Ok(())
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut result = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query){
+            result.push(line);
+        }
+    }
+
+    result
+}
+
+pub struct Config {
+    pub query: String,
+    pub filename: String,
+}
+
+impl Config {
+    pub fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Ok(Config { query, filename })
+    }
+}
+
